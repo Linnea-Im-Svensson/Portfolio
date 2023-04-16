2cbbfb5a536ec08ec7411a8ae5d91596c7266699
ChasAcademy-Linnea-Svensson <113734152+ChasAcademy-Linnea-Svensson@users.noreply.github.com>
2023-04-16 17:11:27 +0200

    added: preloader on contact, fixed: lightmode on contact

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Btn from './btn';
 import { sendContactForm } from '../../lib/api';
+import Image from 'next/image';
 
 export default function ContactForm() {
   const [formInfo, setFormInfo] = useState({
@@ -10,10 +11,13 @@ export default function ContactForm() {
   });
 
   const [showmessage, setShowMessage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async () => {
+    setIsLoading(true);
     await sendContactForm(formInfo);
     setShowMessage(true);
+    setIsLoading(false);
     setTimeout(() => {
       setShowMessage(false);
     }, 4000);
@@ -33,14 +37,16 @@ export default function ContactForm() {
         className='flex flex-col gap-6 px-6 py-8 rounded-xl md:w-1/3 w-full h-fit'
         onSubmit={(e) => (handleSubmit(), e.preventDefault())}
       >
-        <h1 className='text-indigo-200 text-3xl font-semibold'>Contact me</h1>
+        <h1 className='dark:text-indigo-200 text-3xl font-semibold'>
+          Contact me
+        </h1>
         <div className='flex flex-col'>
           <label htmlFor='name'>Name:</label>
           <input
             type='text'
             id='name'
             required
-            className='bg-transparent border-2 rounded-lg p-2'
+            className='dark:bg-transparent border-2 rounded-lg p-2'
             value={formInfo.name}
             onChange={(e) => setFormInfo({ ...formInfo, name: e.target.value })}
           />
@@ -51,7 +57,7 @@ export default function ContactForm() {
             type='email'
             id='email'
             required
-            className='bg-transparent border-2 rounded-lg p-2'
+            className='dark:bg-transparent border-2 rounded-lg p-2'
             value={formInfo.email}
             onChange={(e) =>
               setFormInfo({ ...formInfo, email: e.target.value })
@@ -63,7 +69,7 @@ export default function ContactForm() {
           <textarea
             id='message'
             required
-            className='bg-transparent border-2 rounded-lg md:h-96 h-60 p-2'
+            className='dark:bg-transparent border-2 rounded-lg md:h-96 h-60 p-2'
             value={formInfo.message}
             onChange={(e) =>
               setFormInfo({ ...formInfo, message: e.target.value })
@@ -71,7 +77,13 @@ export default function ContactForm() {
           />
         </div>
         <div className='m-auto'>
-          <Btn style='secondary'>Submit</Btn>
+          <Btn style='secondary'>
+            {isLoading ? (
+              <Image src='/preloader.gif' width={50} height={50} />
+            ) : (
+              'Submit'
+            )}
+          </Btn>
         </div>
       </form>
     </>
